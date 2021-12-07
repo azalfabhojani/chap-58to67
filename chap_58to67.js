a6a7a02259522bdecf3fb18ff8523eed10886dc6
unknown <azalfabhojani921@gmail.com>
2021-12-08 03:35:59 +0500

    Workcompleted

---
+++ chap_58to67.js
@@ -0,0 +1,75 @@
+//i
+//    var main_content = document.getElementById("main-content");
+//    console.log(main_content)
+
+// ii
+//    console.log(main_content.child);
+
+// iii
+//  var render=document.getElementsByClassName("render").innerHTML = "Paragraph changed!";
+//   document.write(render);
+
+// iv
+//   var first_name = document.getElementById("first-name");
+//   var fname = document.createTextNode("azalfa bhojani");
+//      first_name.appendChild(fname);
+//      console.log(first_name);
+
+// v
+//    var last_name = document.getElementById("last-name");
+//    var lname = document.createTextNode("riaz");
+//       last_name.appendChild(lname);
+//       console.log(last_name);
+//    var email = document.getElementById("email");
+//    var emai = document.createTextNode("azalfabhojani921@gmail.com");
+//       email.appendChild(emai);
+//       console.log(email);
+
+
+// i.
+
+//    var form_content =document.getElementById("form-content");
+    //  console.log(form_content.nodeType);
+
+// // ii.
+
+// var lastname =document.getElementById("last-name");
+// console.log(lastname.nodeType);
+// console.log(lastname.childNodes);
+
+
+// // iii.
+
+// var laname =document.getElementById("last-name");
+// console.log(laname);
+
+// // iv. 
+
+// var maincontent =document.get/ElementById("main-content");
+// console.log(maincontent.firstChild);
+// console.log(maincontent.lastChild);
+
+// // v. 
+
+// var lastname =document.getElementById("last-name");
+// console.log(lastname.nextSibling);
+// console.log(lastname.previousSibling);
+
+// vi. 
+
+var email = document.getElementById("email");
+console.log(email.parentNode);
+console.log(email.nodeType);
+
+
+
+
+
+
+
+
+
+
+
+
+
